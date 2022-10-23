def173ffc1cf9382253286f86d941cd89e8dc4f5
nilsdev <nilsdev+1@protonmail.com>
2022-10-23 11:43:55 +0200

    Update .config/kitty/kitty.conf Update install-packages.sh
    Former-commit-id: 95efcaa11eac754e9c1c78217523f4dd05886b4a

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/bash
 
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
 # ┌──────────────────────────────────────────────┐
 # │ check for yay and install if it doesnt exist │
 # └──────────────────────────────────────────────┘
 
+echo    "┌─────────────────────────────────┐"
+echo    "│ checking for packages           │"
+echo -e "│ ${Green}Installed${Color_Off}                       │"
+echo -e "│ ${Red}Not Installed${Color_Off}                   │"
+echo    "└─────────────────────────────────┘"
 if pacman -Q "yay" > /dev/null 2>&1; then 
-	echo -e "yay \t-> is installed" 
+	echo -e "${Green}yay${Color_Off}" 
 else
  	sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git ~/yay && cd ~/yay && makepkg -si -y 
 fi
@@ -39,11 +56,10 @@ packages=(
 
 for package in ${packages[@]}; do 
 	if pacman -Q "$package" > /dev/null 2>&1; then 
-		echo -e "$package \t-> is installed" 
+		# echo -e "$package \t-> is installed" 
+		echo -e "${Green}${package}${Color_Off}" 
 	else 
-		echo -e "$package \t-> has to be installed"
-		echo -e "start installing $package"
+		echo -e "${Red}${package}${Color_Off}" 
 		yay -S "$package" --noconfirm
-		echo -e "end installing $package"
 	fi
 done
