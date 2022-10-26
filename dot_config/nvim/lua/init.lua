eb272208a3a2af2c1210893edc9305850b1f2ddc
nilsdev <nilsdev+1@protonmail.com>
2022-10-26 17:59:46 +0200

    Update .config/nvim/lua/init.lua

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -168,5 +168,3 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
-
-
