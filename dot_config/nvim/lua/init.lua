44082c84872c81d236476a87935931fd3762a4c2
nilsdev <nilsdev+1@protonmail.com>
2022-10-27 15:18:23 +0200

    Update .config/nvim/lua/init.lua

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -186,7 +186,7 @@ require'sniprun'.setup({
 })
 
 local ctp_feline = require('catppuccin.groups.integrations.feline')
-ctp_feline.setup()
+-- ctp_feline.setup()
 
 require("feline").setup({
     components = ctp_feline.get(),
