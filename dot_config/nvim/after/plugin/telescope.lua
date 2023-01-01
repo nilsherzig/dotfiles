927d1b48d83612d550cf3bbef07ee2de89387bad
nilsdev <nilsdev+1@protonmail.com>
2023-01-01 23:00:06 +0100

    test
    Former-commit-id: b9c8facfb5e0391f7f3fb293536d59b058b98df6

---
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -0,0 +1,8 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
