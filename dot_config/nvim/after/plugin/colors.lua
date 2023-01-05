644318a4208871322e93576f94e0ab055b837abc
nilsdev <nilsdev+1@protonmail.com>
2023-01-05 16:28:24 +0100

    Update .config/nvim/after/plugin/colors.lua Update .config/nvim/init.lua Remove .config/nvim/plugin/packer_compiled.lua
    Former-commit-id: f131c888859308b3d4a03612126ac18e5a3308bc

--- dot_config/nvim/after/plugin/colors.lua
+++ dot_config/nvim/after/plugin/colors.lua
@@ -1,8 +1,8 @@
 require('rose-pine').setup({
-    disable_background = true
+    disable_background = false
 })
 
-function ColorMyPencils(color) 
+function ColorMyPencils(color)
 	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
