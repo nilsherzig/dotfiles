8f29aba2aba350ee19e1ab23671c8104671a0387
nilsdev <nilsdev+1@protonmail.com>
2023-02-06 02:55:48 +0100

    Update .config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -1,6 +1,7 @@
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "catppuccin_mocha"
+-- lvim.colorscheme = "catppuccin_mocha"
+lvim.colorscheme = "wal"
 -- lvim.colorscheme = "rose-pine"
 lvim.transparent_window = true
 
@@ -78,6 +79,9 @@ lvim.plugins = {
         }
       )
     end,
+  },
+  {
+    "dylanaraps/wal.vim"
   },
   {
     "nvim-orgmode/orgmode",
