144d163e40917dbb208a9c195995a14e09f75260
nilsherzig <nilsdev+1@protonmail.com>
2023-05-03 14:04:02 +0200

    some wm tools
    Former-commit-id: 912f1255063546a56bb8e92c59a53f231f4f227a

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,9 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      /etc/nixos/hardware-configuration.nix
+    [ 
+      /etc/nixos/hardware-configuration.nix  # Include the results of the hardware scan.
     ];
 
   # Bootloader.
@@ -129,7 +125,7 @@
       signal-desktop
       #games
       steam
-      prismlauncher-qt5
+      # prismlauncher-qt5
       wine
       bottles
       #mail 
@@ -140,7 +136,6 @@
       adw-gtk3
       # hardware / stats
       via       # somehow doesnt work, appimage in repo works on arch tho
-      vial      # more udev testing
       netdata
       radeontop
       liquidctl
@@ -166,11 +161,19 @@
       gnome.sushi
       gnome.gnome-disk-utility
       gnome.gnome-font-viewer
+      # gnome.gnome-control-center
 
       # window manager tools
       wofi
       gammastep
       swaybg
+      pavucontrol
+      brightnessctl
+      libnotify
+      # screenshot stack lel
+      grim
+      slurp  
+      swappy
     ];
   };
 
@@ -178,18 +181,14 @@
 
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    # remotePlay.openFirewall = true; 
+    # dedicatedServer.openFirewall = true; 
   };
 
-  # hyprland / wm configs 
   programs.hyprland.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     htop
     neovim 
@@ -201,35 +200,9 @@
     cantarell-fonts
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
   services.netdata.enable = true;
   services.mullvad-vpn.enable = true; 
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  system.stateVersion = "22.11"; # Did you read the comment? # na i didnt, going to change this anyways
 
   system.autoUpgrade.enable = true;
   system.autoUpgrade.allowReboot = false;
