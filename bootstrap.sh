3b6c56b67f89a373fd90ccc827797ebf24a3245a
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 17:59:33 +0200

    switch to unstable bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 set -xe 
+sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+nixos-rebuild switch --upgrade
+
+sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
 sudo rm "/etc/nixos/configuration.nix"
 ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+nixos-rebuild switch --upgrade
