c819025d9b934ef7289144a74e7d2974b418ec59
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 16:35:07 +0200

    moved luks to different file
    Former-commit-id: 131cb99ec22df0bae9215a510bc7d9d3869fea9d

---
+++ nixos/luks.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".device = "/dev/disk/by-uuid/c9eb19e3-05fa-4057-a251-60d49d38de4c";
+  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".keyFile = "/crypto_keyfile.bin";
+}
