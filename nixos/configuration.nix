8b527c270d47bfe72e1d02234f1a29fc0b7ee607
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 16:35:07 +0200

    moved luks to different file
    Former-commit-id: 46659fe9ea15fb8ae571f21473c329fe4a3c517e

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,6 +4,7 @@
   imports =
     [ 
       /etc/nixos/hardware-configuration.nix  # Include the results of the hardware scan.
+      ./luks.nix
     ];
 
   # Bootloader.
@@ -20,8 +21,8 @@
   };
 
   # Enable swap on luks
-  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".device = "/dev/disk/by-uuid/c9eb19e3-05fa-4057-a251-60d49d38de4c";
-  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".keyFile = "/crypto_keyfile.bin";
+  # boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".device = "/dev/disk/by-uuid/c9eb19e3-05fa-4057-a251-60d49d38de4c";
+  # boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".keyFile = "/crypto_keyfile.bin";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
