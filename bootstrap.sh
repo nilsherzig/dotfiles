a43226fe3cbf68886245cdc0cc8de9c5cb43944e
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 18:04:46 +0200

    sudo
    Former-commit-id: ffc1e666b33b71fc0b77cb8ef98db720395181f3

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -xe 
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
-nixos-rebuild switch --upgrade
+sudo nixos-rebuild switch --upgrade
 
 # sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
 # sudo rm "/etc/nixos/configuration.nix"
-# ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+# sudo ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
 # nixos-rebuild switch --upgrade
