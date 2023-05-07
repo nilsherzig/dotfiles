a93b8b17ec057d4938e373f62704e05db427a29d
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 22:11:05 +0200

    homemanager
    Former-commit-id: cb033990baa45876537bde44d47004caddc08b91

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,9 @@ set -xe
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nixos-rebuild switch --upgrade
 
+sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home
+sudo nix-channel --update
+
 # sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
 # sudo rm "/etc/nixos/configuration.nix"
 # sudo ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
