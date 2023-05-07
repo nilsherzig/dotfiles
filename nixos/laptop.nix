69555776cef8379261c2898c280bdb3828ce5b5f
nilsherzig <nils@nilsherzig.com>
2023-05-07 21:18:36 +0200

    laptop
    Former-commit-id: bc8269ae0ba585e45dc873a3413577877c5a3937

--- nixos/laptop.nix
+++ nixos/laptop.nix
@@ -1,6 +1,6 @@
  {config, pkgs, lib, ...}:
 lib.mkIf (config.networking.hostName == "laptop") {
   # Enable swap on luks
-  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".device = "/dev/disk/by-uuid/c9eb19e3-05fa-4057-a251-60d49d38de4c";
-  boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".keyFile = "/crypto_keyfile.bin";
- }
+  boot.initrd.luks.devices."luks-f01a4a17-fb5d-4c97-81c4-4968d987c0d7".device = "/dev/disk/by-uuid/f01a4a17-fb5d-4c97-81c4-4968d987c0d7";
+  boot.initrd.luks.devices."luks-f01a4a17-fb5d-4c97-81c4-4968d987c0d7".keyFile = "/crypto_keyfile.bin";
+}
