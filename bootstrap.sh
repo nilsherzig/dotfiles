1db221a0100471b133770f252f15855d09643e82
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 18:02:15 +0200

    test

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -xe
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 nixos-rebuild switch --upgrade
 
-sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
-sudo rm "/etc/nixos/configuration.nix"
-ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
-nixos-rebuild switch --upgrade
+# sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
+# sudo rm "/etc/nixos/configuration.nix"
+# ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+# nixos-rebuild switch --upgrade
