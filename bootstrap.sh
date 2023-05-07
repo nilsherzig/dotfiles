4dc4276536c93f4d1d7ac69b9d067e7ce30b0b04
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 23:07:08 +0200

    disabled atuin
    Former-commit-id: 96ad71c550ebf76f8abf63f99f6d19b401f1ee27

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env sh
+
+# i hate that this exists, plan is to move everything into nixos
 set -xe 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nixos-rebuild switch --upgrade
 
-sudo nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home
-sudo nix-channel --update
-sudo nixos-rebuild switch
-
-nix-shell '<home-manager>' -A install
-ln -s "$SCRIPTPATH"/home-manager /home/"$USER"/.config/
-home-manager switch
-
-# sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
-# sudo rm "/etc/nixos/configuration.nix"
-# sudo ln -s "$PWD/nixos/configuration.nix" "/etc/nixos/configuration.nix"
-# nixos-rebuild switch --upgrade
+sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
+sudo rm "/etc/nixos/configuration.nix"
+sudo ln -s "$SCRIPTPATH/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+nixos-rebuild switch --upgrade
