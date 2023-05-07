c59af31b64750861e7245eba87ddcf852b1ce8ee
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 22:28:53 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -xe 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nixos-rebuild switch --upgrade
 
@@ -8,6 +10,8 @@ sudo nix-channel --update
 sudo nixos-rebuild switch
 
 nix-shell '<home-manager>' -A install
+ln -s "$SCRIPTPATH"/home-manager /home/"$USER"/.config/
+home-manager switch
 
 # sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
 # sudo rm "/etc/nixos/configuration.nix"
