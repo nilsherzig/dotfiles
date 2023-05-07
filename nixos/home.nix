ae4f371e6b9bf412125cd989db1d6ffda27ce7be
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 23:01:15 +0200

    firefox gnome theme
    Former-commit-id: ea4e0cf47e859e9750c86795ce6b1b5d0ca7a3c6

--- nixos/home.nix
+++ nixos/home.nix
@@ -24,6 +24,30 @@ in
             package = pkgs.adw-gtk3;
         };
     };
+
+    home.file."firefox-gnome-theme" = {
+        target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
+        source = (fetchTarball "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz");
+    };
+
+    programs.firefox = {
+        enable = true;
+        profiles.default = {
+            name = "Default";
+            settings = {
+                "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+                # For Firefox GNOME theme:
+                "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+                "browser.tabs.drawInTitlebar" = true;
+                "svg.context-properties.content.enabled" = true;
+            };
+            userChrome = ''
+                @import "firefox-gnome-theme/userChrome.css";
+                @import "firefox-gnome-theme/theme/colors/dark.css"; 
+            '';
+        };
+    };
     /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
   };
 }
