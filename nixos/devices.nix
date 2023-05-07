6453088c2748c3526fc8873bd22f34afeade777e
nilsherzig <nilsdev+1@protonmail.com>
2023-05-07 17:17:24 +0200

    device settings

--- nixos/devices.nix
+++ nixos/devices.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, ... }:
+ {config, pkgs, lib, ...}:
 
-{
+lib.mkIf (config.networking.hostName == "desktop") {
   # Enable swap on luks
   boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".device = "/dev/disk/by-uuid/c9eb19e3-05fa-4057-a251-60d49d38de4c";
   boot.initrd.luks.devices."luks-c9eb19e3-05fa-4057-a251-60d49d38de4c".keyFile = "/crypto_keyfile.bin";
+
+  fileSystems."/data" =
+  {
+    device = "/dev/disk/by-label/ssd1tb";
+    fsType = "ext4";
+  };
 }
