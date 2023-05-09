e3290dd1e55c4f574691b6b50e8501b7ccdb6821
nilsherzig <nilsdev+1@protonmail.com>
2023-05-09 22:51:09 +0200

    script changes
    Former-commit-id: 796b77b0d042bff86d3d487a2d42f97cdd7edb22

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -9,8 +9,10 @@ sudo liquidctl initialize all
 export color1=231942
 export color2=5E548E
 
-sudo liquidctl --match Commander set sync color clear
-sudo liquidctl --match Commander set sync color color_shift $color1 $color2 --start-led 0 
+# sudo liquidctl --match Commander set sync color clear
+sudo liquidctl --match Commander set sync color off
+# sudo liquidctl --match Commander set sync color color_shift $color1 $color2 --start-led 0 
 
-sudo liquidctl --match H100i set led off
-sudo liquidctl --match H100i set led color fixed $color1
+
+sudo liquidctl set led color off --match H100i 
+# sudo liquidctl --match H100i set led color fixed $color1
