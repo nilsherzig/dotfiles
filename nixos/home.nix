8f58afe55a93532a1c6efb3b8e9547ecad567289
nilsherzig <nilsdev+1@protonmail.com>
2023-05-10 16:56:18 +0200

    added rancher desktop to path
    Former-commit-id: f6fe1855c358b42d700953477ee1d4263898f285

--- nixos/home.nix
+++ nixos/home.nix
@@ -11,41 +11,44 @@ in
     home.stateVersion = "22.11";
 
     home.file.".config/" = {
-        source = ../configs;
-        recursive = true;
+      source = ../configs;
+      recursive = true;
     };
 
     gtk = {
-        enable = true;
-        theme = {
-            name = "adw-gtk3";
-            package = pkgs.adw-gtk3;
-        };
+      enable = true;
+      theme = {
+        name = "adw-gtk3";
+        package = pkgs.adw-gtk3;
+      };
     };
+    home.sessionPath = [
+      "$HOME/.rd/bin"
+    ]
 
-    home.file."firefox-gnome-theme" = {
-        target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
-        source = (fetchTarball "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz");
-    };
-
-    programs.firefox = {
-        enable = true;
-        profiles.default = {
-            name = "Default";
-            settings = {
-                "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-
-                # For Firefox GNOME theme:
-                "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-                "browser.tabs.drawInTitlebar" = true;
-                "svg.context-properties.content.enabled" = true;
-            };
-            userChrome = ''
-                @import "firefox-gnome-theme/userChrome.css";
-                @import "firefox-gnome-theme/theme/colors/dark.css"; 
-            '';
-        };
-    };
-    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+      home.file."firefox-gnome-theme" = {
+    target = ".mozilla/firefox/default/chrome/firefox-gnome-theme";
+    source = (fetchTarball "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/master.tar.gz");
   };
+
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      name = "Default";
+      settings = {
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+        # For Firefox GNOME theme:
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.tabs.drawInTitlebar" = true;
+        "svg.context-properties.content.enabled" = true;
+      };
+      userChrome = ''
+        @import "firefox-gnome-theme/userChrome.css";
+        @import "firefox-gnome-theme/theme/colors/dark.css"; 
+      '';
+    };
+  };
+  /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+};
 }
