bcbfd397f4cdb87d24042519c0b771bbad7ec23b
nilsherzig <nilsdev+1@protonmail.com>
2023-05-19 16:15:46 +0000

    neovim plugings

---
+++ nvim/after/plugin/wilder.lua
@@ -0,0 +1,12 @@
+wilder = require("wilder");
+
+-- wilder.set_option('renderer', wilder.popupmenu_renderer(
+--     wilder.popupmenu_border_theme({
+--         highlighter = wilder.basic_highlighter(),
+--         left = {' ', wilder.popupmenu_devicons()},
+--         -- right = {' ', wilder.popupmenu_scrollbar()},
+--         -- min_width = '100%', -- minimum height of the popupmenu, can also be a number
+--         -- min_height = '50%', -- to set a fixed height, set max_height to the same value
+--         -- reverse = 0,        -- if 1, shows the candidates from bottom to top
+--     })
+-- ))
