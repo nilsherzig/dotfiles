0d351dcb3dc168fc43796e3876a95bdfc948376b
nilsherzig <nilsdev+1@protonmail.com>
2023-05-19 20:15:08 +0200

    fedora devbox setup script

--- nvim/setup_fedora.sh
+++ nvim/setup_fedora.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 set -xe
 
-sudo apt-get install -y build-essential
+cat << EOF | sudo tee /etc/dnf/dnf.conf
+[main]
+gpgcheck=True
+installonly_limit=3
+clean_requirements_on_remove=True
+best=False
+skip_if_unavailable=True
+max_parallel_downloads=20
+EOF
+
+# install base dev tools and libs
+sudo dnf groupinstall -y "Development Tools" "Development Libraries"
 
 # basic tools and package managers which are needed to download the lsp servers
-sudo apt-get install -y make automake gcc git nodejs cargo
+sudo dnf install -y make automake gcc git nodejs cargo
 
 # msp430 bullshit
 sudo dnf copr enable nielsenb/msp430-development-tools -y
-sudo dnf install -y msp430-elf-gcc msp430-elf-binutils msp430-elf-gdb mspds msp430flasher msp430-gcc-support-files dos2unix libusb readline
+sudo dnf install -y msp430-elf-gcc msp430-elf-binutils msp430-elf-gdb mspds msp430flasher msp430-gcc-support-files dos2unix libusb readline mspdebug
 
-cd /tmp
-git clone https://github.com/dlbeer/mspdebug
-cd /tmp/mspdebug
-
-distrobox-export --bin /usr/bin/nvim --export-path ~/.local/bin
+# export neovim to host system - everything inside neovim will run inside the container
+distrobox-export --bin /usr/bin/nvim --export-path ~/.local/bin/
