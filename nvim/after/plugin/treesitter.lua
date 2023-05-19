ceb4dc8dbd573c74b1e498ebd51b4a9f32aa657f
nilsherzig <nilsdev+1@protonmail.com>
2023-05-19 14:31:07 +0200

    custom neovim config, has to be symlinked
    Former-commit-id: dd10fe13d91dbddbdba6379e101eb5c108b9b16c

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,13 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the five listed parsers should always be installed)
+  ensure_installed = { all },
+
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
