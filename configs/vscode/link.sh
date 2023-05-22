b54b393dbd3c4ae3741fb3bce6ba100a92b7faed
nilsherzig <nilsdev+1@protonmail.com>
2023-05-23 00:32:43 +0200

    moved some files + link script + new neovim colors

--- configs/vscode/link.sh
+++
@@ -1,2 +0,0 @@
-ln -sf $PWD/extensions.js $HOME/.vscode/extensions/extensions.json
-ln -sf $PWD/settings.json $HOME/.config/Code/User/settings.json
--- nvim/after/plugin/colors.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-vim.opt.termguicolors = true
-vim.g.sonokai_style = 'shusia'
-vim.g.sonokai_better_performance = 1
-vim.cmd('colorscheme sonokai')
