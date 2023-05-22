ba87fba21d986b2561e3945b6d6ad744d29cbc07
nilsherzig <nilsdev+1@protonmail.com>
2023-05-23 00:44:37 +0200

    color and blankline fixes
    Former-commit-id: fc07c976117d192633783e6efa8ab69ad53dc08c

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,16 @@
 set -xe 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# change nixos channel to unstable
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
+# update
 sudo nixos-rebuild switch --upgrade
 
+# create backup of nix config
 sudo cp "/etc/nixos/configuration.nix" "/etc/nixos/configuration.nix.bak"
-sudo rm "/etc/nixos/configuration.nix"
-sudo ln -s "$SCRIPTPATH/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+
+# link 
+sudo ln -sf "$SCRIPTPATH/nixos/configuration.nix" "/etc/nixos/configuration.nix"
+
+# rebuild
 nixos-rebuild switch --upgrade
