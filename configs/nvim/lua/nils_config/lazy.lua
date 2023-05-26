c85758621efa684aa83c36d0b2ff05a425d3f06e
nilsherzig <nilsdev+1@protonmail.com>
2023-05-26 13:18:17 +0200

    things :))))
    Former-commit-id: 398a1107e43fba3b044b834b367270c5cc5153fc

--- configs/nvim/lua/nils_config/lazy.lua
+++ configs/nvim/lua/nils_config/lazy.lua
@@ -1,197 +1,201 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "lukas-reineke/indent-blankline.nvim" },
+	{ "lukas-reineke/indent-blankline.nvim" },
 
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            })
-        end,
-    },
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		-- config = function()
+		--     require("null-ls").setup()
+		-- end,
+		-- requires = { "nvim-lua/plenary.nvim" },
+	},
 
-    {
-        "yamatsum/nvim-cursorline",
-        config = function()
-            require('nvim-cursorline').setup {
-                cursorline = {
-                    enable = false,
-                    timeout = 1000,
-                    number = false,
-                },
-                cursorword = {
-                    enable = true,
-                    min_length = 3,
-                    hl = { underline = true },
-                }
-            }
-        end
-    },
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	},
 
-    "gelguy/wilder.nvim",
+	{
+		"yamatsum/nvim-cursorline",
+		config = function()
+			require("nvim-cursorline").setup({
+				cursorline = {
+					enable = false,
+					timeout = 1000,
+					number = false,
+				},
+				cursorword = {
+					enable = true,
+					min_length = 3,
+					hl = { underline = true },
+				},
+			})
+		end,
+	},
 
-    "folke/neodev.nvim",
+	"gelguy/wilder.nvim",
 
-    "jiangmiao/auto-pairs",
+	"folke/neodev.nvim",
 
+	"jiangmiao/auto-pairs",
 
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup()
-        end
-    },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
 
-    {
-        "terrortylor/nvim-comment",
-        config = function()
-            require('nvim_comment').setup()
-        end,
+	{
+		"terrortylor/nvim-comment",
+		config = function()
+			require("nvim_comment").setup()
+		end,
+	},
 
-    },
+	"sainnhe/sonokai",
 
-    "sainnhe/sonokai",
+	-- {
+	--     "projekt0n/github-nvim-theme",
+	--     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+	--     priority = 1000, -- make sure to load this before all the other start plugins
+	--     config = function()
+	--         require('github-theme').setup({
+	--             -- ...
+	--         })
+	--
+	--         vim.cmd('colorscheme github_dark_high_contrast')
+	--     end,
+	-- },
+	--
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = {
+					-- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true,
+				show_end_of_buffer = true, -- show the '~' characters after the end of buffers
+				term_colors = false,
+				dim_inactive = {
+					enabled = false,
+					shade = "dark",
+					percentage = 0.15,
+				},
+				no_italic = false, -- Force no italic
+				no_bold = false, -- Force no bold
+				no_underline = false, -- Force no underline
+				styles = {
+					comments = { "italic" },
+					conditionals = { "italic" },
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+					operators = {},
+				},
+				color_overrides = {},
+				custom_highlights = {},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					telescope = true,
+					notify = false,
+					mini = false,
+				},
+			})
 
-    -- {
-    --     "projekt0n/github-nvim-theme",
-    --     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    --     priority = 1000, -- make sure to load this before all the other start plugins
-    --     config = function()
-    --         require('github-theme').setup({
-    --             -- ...
-    --         })
-    --
-    --         vim.cmd('colorscheme github_dark_high_contrast')
-    --     end,
-    -- },
-    --
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup({
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                background = {
-                    -- :h background
-                    light = "latte",
-                    dark = "mocha",
-                },
-                transparent_background = true,
-                show_end_of_buffer = true, -- show the '~' characters after the end of buffers
-                term_colors = false,
-                dim_inactive = {
-                    enabled = false,
-                    shade = "dark",
-                    percentage = 0.15,
-                },
-                no_italic = false,    -- Force no italic
-                no_bold = false,      -- Force no bold
-                no_underline = false, -- Force no underline
-                styles = {
-                    comments = { "italic" },
-                    conditionals = { "italic" },
-                    loops = {},
-                    functions = {},
-                    keywords = {},
-                    strings = {},
-                    variables = {},
-                    numbers = {},
-                    booleans = {},
-                    properties = {},
-                    types = {},
-                    operators = {},
-                },
-                color_overrides = {},
-                custom_highlights = {},
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    telescope = true,
-                    notify = false,
-                    mini = false,
-                },
-            })
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 
-            vim.cmd.colorscheme "catppuccin"
-        end
-    },
+	{
+		"iamcco/markdown-preview.nvim",
+		config = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
-    {
-        "iamcco/markdown-preview.nvim",
-        config = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
+	{
+		"preservim/vim-markdown",
+		require = { "godlygeek/tabular" },
+	},
 
-    {
-        'preservim/vim-markdown',
-        require = { 'godlygeek/tabular' },
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate"
-    },
+	-- "mbbill/undotree",
 
-    -- "mbbill/undotree",
+	"tpope/vim-fugitive",
 
-    "tpope/vim-fugitive",
+	{
+		"andrewferrier/wrapping.nvim",
+		config = function()
+			require("wrapping").setup()
+		end,
+	},
 
-    {
-        "andrewferrier/wrapping.nvim",
-        config = function()
-            require("wrapping").setup()
-        end
-    },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		lazy = false,
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{
+				-- Optional
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        lazy = false,
-        branch = 'v2.x',
-        dependencies = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
-            {
-                -- Optional
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
-            },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
-        },
-
-    }
-
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	},
 })
 
-local wilder = require('wilder')
-wilder.setup({ modes = { ':', '/', '?' } })
+local wilder = require("wilder")
+wilder.setup({ modes = { ":", "/", "?" } })
