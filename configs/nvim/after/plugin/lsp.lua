e9d971fa3afe3fee9fabd7cbf4aa4d0322461b7d
nilsherzig <nilsdev+1@protonmail.com>
2023-06-11 15:54:54 +0200

    things

--- configs/nvim/after/plugin/lsp.lua
+++ configs/nvim/after/plugin/lsp.lua
@@ -20,12 +20,16 @@ lsp.preset("recommended")
 --     -- "gopls"
 -- })
 
-require('lspconfig').gopls.setup {}            -- installed via nixpkgs
-require('lspconfig').bashls.setup {}           -- installed via nixpkgs
-require('lspconfig').lua_ls.setup {}           -- installed via nixpkgs
-require('lspconfig').nil_ls.setup {}           -- installed via nixpkgs
-require('lspconfig').rust_analyzer.setup {}    -- installed via nixpkgs
--- require('lspconfig').jdtls.setup{}  -- installed via nixpkgs
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+require('lspconfig').gopls.setup {}         -- installed via nixpkgs
+require('lspconfig').bashls.setup {}        -- installed via nixpkgs
+require('lspconfig').lua_ls.setup {}        -- installed via nixpkgs
+require('lspconfig').nil_ls.setup {}        -- installed via nixpkgs
+require('lspconfig').rust_analyzer.setup {} -- installed via nixpkgs
+require('lspconfig').marksman.setup {}      -- installed via nixpkgs
+require('lspconfig').html.setup {}          -- installed via nixpkgs
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
@@ -58,41 +62,41 @@ lsp.set_preferences({
 
 -- lsp.on_attach(function(client, bufnr)
 --     local opts = { buffer = bufnr, remap = false }
-    local opts = { remap = false }
+local opts = { remap = false }
 
-    vim.keymap.set("n", "gd", function()
-        vim.lsp.buf.definition()
-    end, opts)
-    vim.keymap.set("n", "K", function()
-        vim.lsp.buf.hover()
-    end, opts)
-    vim.keymap.set("n", "<leader>vws", function()
-        vim.lsp.buf.workspace_symbol()
-    end, opts)
-    vim.keymap.set("n", "<leader>lf", function()
-        vim.lsp.buf.format()
-    end, opts)
-    vim.keymap.set("n", "<leader>vd", function()
-        vim.diagnostic.open_float()
-    end, opts)
-    vim.keymap.set("n", "<leader>ln", function()
-        vim.diagnostic.goto_next()
-    end, opts)
-    vim.keymap.set("n", "<leader>lp", function()
-        vim.diagnostic.goto_prev()
-    end, opts)
-    vim.keymap.set("n", "<leader>la", function()
-        vim.lsp.buf.code_action()
-    end, opts)
-    vim.keymap.set("n", "<leader>vrr", function()
-        vim.lsp.buf.references()
-    end, opts)
-    vim.keymap.set("n", "<leader>lr", function()
-        vim.lsp.buf.rename()
-    end, opts)
-    vim.keymap.set("i", "<C-h>", function()
-        vim.lsp.buf.signature_help()
-    end, opts)
+vim.keymap.set("n", "gd", function()
+    vim.lsp.buf.definition()
+end, opts)
+vim.keymap.set("n", "K", function()
+    vim.lsp.buf.hover()
+end, opts)
+vim.keymap.set("n", "<leader>vws", function()
+    vim.lsp.buf.workspace_symbol()
+end, opts)
+vim.keymap.set("n", "<leader>lf", function()
+    vim.lsp.buf.format()
+end, opts)
+vim.keymap.set("n", "<leader>vd", function()
+    vim.diagnostic.open_float()
+end, opts)
+vim.keymap.set("n", "<leader>ln", function()
+    vim.diagnostic.goto_next()
+end, opts)
+vim.keymap.set("n", "<leader>lp", function()
+    vim.diagnostic.goto_prev()
+end, opts)
+vim.keymap.set("n", "<leader>la", function()
+    vim.lsp.buf.code_action()
+end, opts)
+vim.keymap.set("n", "<leader>vrr", function()
+    vim.lsp.buf.references()
+end, opts)
+vim.keymap.set("n", "<leader>lr", function()
+    vim.lsp.buf.rename()
+end, opts)
+vim.keymap.set("i", "<C-h>", function()
+    vim.lsp.buf.signature_help()
+end, opts)
 -- end)
 
 lsp.setup()
