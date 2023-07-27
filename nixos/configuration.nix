c7c1e317d57b81447160e06d58cf28a35c1cd70b
nilsherzig <nils@nilsherzig.com>
2023-07-27 23:45:30 +0200

    removed space
    Former-commit-id: 1548cf2de8b669fe0dd27b9451ce783749b182c1

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -475,4 +475,17 @@
   services.duplicity.enable = true; 
   services.duplicity.frequency = null; 
   services.duplicity.targetUrl = "";
+
+environment.sessionVariables = rec {
+    XDG_CACHE_HOME  = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME   = "$HOME/.local/share";
+    XDG_STATE_HOME  = "$HOME/.local/state";
+
+    # Not officially in the specification
+    XDG_BIN_HOME    = "$HOME/.local/bin";
+    PATH = [ 
+      "${XDG_BIN_HOME}"
+    ];
+  };
 }
