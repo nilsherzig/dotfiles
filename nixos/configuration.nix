f939430d38234b7a8bb293afb0cda27474653023
nilsherzig <nilsdev+1@protonmail.com>
2023-07-27 22:10:16 +0200

    enabled gnome
    Former-commit-id: 56517a6c25badcb02c0ba4a9c98a4a9f6b131db2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -81,8 +81,8 @@
   # services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # because via and keyboard configs # doesnt work rn 
   # the via and vial packages already do these in their install scripts, idk why they dont work
