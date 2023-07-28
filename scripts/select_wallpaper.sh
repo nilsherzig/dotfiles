16f0b4644f8037613b1e79c4c331c8560a29b30c
nilsherzig <nilsdev+1@protonmail.com>
2023-07-28 11:30:53 +0200

    things
    Former-commit-id: 6c5e65a0d05a213289e97a28a1ffc1b7e2480331

--- scripts/select_wallpaper.sh
+++ scripts/select_wallpaper.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 pkill swaybg
-cp ~/Pictures/wallpaper/$(ls ~/Pictures/wallpaper/ | fzf --preview="firefox ~/Pictures/wallpaper/{}") ~/Pictures/wallpaper/currentwallpaper
+cp ~/Pictures/wallpaper/$(ls ~/Pictures/wallpaper/ | fzf --preview="kitty icat --clear --place 200x40@0x0 --transfer-mode file ~/Pictures/wallpaper/{}") ~/Pictures/wallpaper/currentwallpaper
 swaybg -i ~/Pictures/wallpaper/currentwallpaper -m fill & 
