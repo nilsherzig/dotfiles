77b87aa795ad6b81b4512740bc67dc9616f16696
nilsherzig <nils@nilsherzig.com>
2023-09-13 09:36:37 +0200

    neovvim

--- configs/nvim/after/plugin/telescope.lua
+++ configs/nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>s', builtin.lsp_workplace_symbols, {})
