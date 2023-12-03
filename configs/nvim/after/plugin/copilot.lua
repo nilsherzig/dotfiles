9423cb960850b2cc54ef3286070cc220b387e067
Nils Herzig <git@nilsherzig.com>
2023-12-03 16:40:02 +0100

    asad

--- configs/nvim/after/plugin/copilot.lua
+++ configs/nvim/after/plugin/copilot.lua
@@ -11,44 +11,49 @@
 --     ['css'] = true,
 -- }
 -- require("copilot").setup(options)
-require('copilot').setup({
-    filetypes = {
-        yaml = true,
-        markdown = true,
-        go = true,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-    },
-
-    suggestion = {
-        enabled = false,
-        -- auto_trigger = true,
-        -- debounce = 75,
-        -- keymap = {
-        --     accept = "<tab>",
-        --     accept_word = false,
-        --     accept_line = false,
-        --     next = "<M-]>",
-        --     prev = "<M-[>",
-        --     dismiss = "<C-]>",
-        -- },
-    },
-    -- suggestion = { enabled = true },
+--
+require("copilot").setup({
+    suggestion = { enabled = false },
     panel = { enabled = false },
-    server_opts_overrides = {
-        -- trace = "verbose",
-        settings = {
-            advanced = {
-                -- listCount = 10, -- #completions for panel
-                inlineSuggestCount = 3, -- #completions for getCompletions
-            }
-        },
-    }
 })
+-- require('copilot').setup({
+--     filetypes = {
+--         yaml = true,
+--         markdown = true,
+--         go = true,
+--         help = false,
+--         gitcommit = false,
+--         gitrebase = false,
+--         hgcommit = false,
+--         svn = false,
+--         cvs = false,
+--         ["."] = false,
+--     },
+--
+--     suggestion = {
+--         enabled = false,
+--         -- auto_trigger = true,
+--         -- debounce = 75,
+--         -- keymap = {
+--         --     accept = "<tab>",
+--         --     accept_word = false,
+--         --     accept_line = false,
+--         --     next = "<M-]>",
+--         --     prev = "<M-[>",
+--         --     dismiss = "<C-]>",
+--         -- },
+--     },
+--     -- suggestion = { enabled = true },
+--     panel = { enabled = false },
+--     server_opts_overrides = {
+--         -- trace = "verbose",
+--         settings = {
+--             advanced = {
+--                 -- listCount = 10, -- #completions for panel
+--                 inlineSuggestCount = 3, -- #completions for getCompletions
+--             }
+--         },
+--     }
+-- })
 
 require("copilot_cmp").setup()
