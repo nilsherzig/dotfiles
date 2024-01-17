a9e1abd28928cddb01762a9430c97d6d5c218715
Nils Herzig <git@nilsherzig.com>
2024-01-17 23:42:10 +0100

    🍎

--- nixos/keyd.nix
+++ nixos/keyd.nix
@@ -3,11 +3,33 @@
     enable = true;
     keyboards."*".settings = {
       main = {
-        # capslock = "layer(caps)";
-        # rightalt = "layer(rightalt)";
-        rightalt = "layer(altgr)";
-        space = "overload(space, shift)";
-        capslock = "esc";
+        j = "overloadt(modLayerOne, j, 300)";
+        f = "overloadt(modLayerOne, f, 300)";
+
+        k = "overloadt(control, k, 300)";
+        d = "overloadt(control, d, 300)";
+
+        s = "overloadt(altgr, s, 300)";
+        l = "overloadt(altgr, l, 300)";
+
+        caps = "overload(caps, caps)";
+      };
+
+      modLayerOne = {
+        # chars use position on us layout for some reason
+        "1" = "!";
+        "2" = "@";
+        "3" = "#";
+        "4" = "$";
+        "5" = "%";
+        "6" = "^";
+        "7" = "&";
+        "8" = "*";
+        "9" = "(";
+        "0" = ")";
+        "," = "<";
+        "." = ">";
+        "m" = "backspace";
       };
 
       altgr = {
