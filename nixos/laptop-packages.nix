d30d27c7923bf1fe8a5b7ec72f715cd6c3d15c23
Nils Herzig <git@nilsherzig.com>
2024-01-29 19:32:53 +0100

    asd

--- nixos/laptop-packages.nix
+++ nixos/laptop-packages.nix
@@ -20,7 +20,7 @@ in {
   nixpkgs.config.permittedInsecurePackages = [ "electron-25.9.0" ];
 
   users.users.nils.packages = with pkgs; [
-    factorio
+    # factorio
     asciinema
     zoxide
     eza
