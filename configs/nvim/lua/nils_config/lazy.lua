48fddd2fbec42b0184a58c767d69c1e93e3049cd
Nils Herzig <git@nilsherzig.com>
2025-02-11 13:23:05 +0100

    adding copilot chat

--- configs/nvim/lua/nils_config/lazy.lua
+++ configs/nvim/lua/nils_config/lazy.lua
@@ -110,6 +110,18 @@ require("lazy").setup({
 		"github/copilot.vim",
 	},
 	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
+	{
 		"someone-stole-my-name/yaml-companion.nvim",
 		dependencies = {
 			"neovim/nvim-lspconfig",
