de9be008ad93873822f9884e13dfa0ae01b40ae2
Nils Herzig <git@nilsherzig.com>
2025-08-19 07:20:55 +0200

    add lazygit plugin

--- configs/nvim/lua/nils_config/lazy.lua
+++ configs/nvim/lua/nils_config/lazy.lua
@@ -75,6 +75,29 @@ require("lazy").setup({
     {
         "brenoprata10/nvim-highlight-colors",
     },
+    -- {
+    --     "tpope/vim-fugitive",
+    -- },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
     {
         "neovim/nvim-lspconfig",
     },
